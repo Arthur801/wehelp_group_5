9ab38033e6b856e1ce0664969a7352975eaebc33
Arthur801 <59018955+Arthur801@users.noreply.github.com>
2026-09-21 15:28:17 +0800

    Initiate main.py with fastapi

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", include_in_schema=False)
+async def index(request: Request):
+	return FileResponse("./views/index.html", media_type="text/html")
