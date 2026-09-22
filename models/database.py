2e07363b9904782a44f504d84cd0c7de5ddfd15c
許純宜 <irenehsu56@gmail.com>
2026-09-22 10:29:00 +0800

    Add air quality database integration

--- models/database.py
+++ models/database.py
@@ -1,1 +1,25 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
 
+load_dotenv()
+
+def get_connection():
+    connection = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+
+    return connection
+
+
+if __name__ == "__main__":
+    connection = get_connection()
+
+    if connection.is_connected():
+        print("資料庫連線成功")
+        connection.close()
+    else:
+        print("資料庫連線失敗")
